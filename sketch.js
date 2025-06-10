8c06aa6f91b200040eaff08ded574a74eafb97ac
DdensityM <150703473+DdensityM@users.noreply.github.com>
2025-06-10 17:18:49 +0200

    Update sketch.js
    画布修改1100*900

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(1100, 900);
   textAlign(CENTER, BASELINE);
   textSize(fontSize);
   textFont('Times New Roman');
