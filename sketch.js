20172cb1e38a4b80c29b9309d83f30cdf6b5999b
DdensityM <150703473+DdensityM@users.noreply.github.com>
2025-06-10 16:38:19 +0200

    Update sketch.js
    修改了画布大小

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1440, 900);
+  createCanvas(1228, 900);
   textAlign(CENTER, BASELINE);
   textSize(fontSize);
   textFont('Times New Roman');
